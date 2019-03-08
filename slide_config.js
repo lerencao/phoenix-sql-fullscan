b65295d501e62a49e1763d612b711f583e998264
caojiafeng <funfriendcjf@gmail.com>
2019-03-08 14:47:48 +0800

    init commit

---
+++ slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'Phoenix SQL 全表扫描检查', 
+     subtitle: 'A Harder Way', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/emacs-icon.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: '长扬', 
+     company: 'wacai', 
+     gplus: '', 
+     twitter: 'lerencao', 
+     www: '', 
+     github: 'http://github.com/lerencao', 
+   }]
+};
